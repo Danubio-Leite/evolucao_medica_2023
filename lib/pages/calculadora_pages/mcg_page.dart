9da6e4f8ebca9f514cfc43895512131550bdcdf9
Danubio-Leite <danubio.dm@hotmail.com>
2023-10-08 17:15:28 -0300

    calculadora mcg

--- lib/pages/calculadora_pages/mcg_page.dart
+++ lib/pages/calculadora_pages/mcg_page.dart
@@ -17,13 +17,17 @@ class _MCGPageState extends State<MCGPage> {
     filter: {'#': RegExp(r'[0-9]')},
   );
   final TextEditingController? _controllerMlHr = TextEditingController();
-  final TextEditingController? _controllerHeight = TextEditingController();
+  final TextEditingController? _controllerWeight = TextEditingController();
+  final TextEditingController? _controllerMgSoro = TextEditingController();
+  final TextEditingController? _controllerMlSoro = TextEditingController();
 
   double? _mlHr = 0;
   double? _weight = 0;
   double? _mgSoro = 0;
   double? _mlSoro = 0;
-  double? _imc = 0;
+  double? _mgml = 0;
+  double? _mgHr = 0;
+  double? _mcgKgMin = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -38,8 +42,11 @@ class _MCGPageState extends State<MCGPage> {
       body: SingleChildScrollView(
         child: Container(
           color: const Color.fromARGB(178, 95, 189, 226),
-          padding:
-              const EdgeInsets.only(bottom: 50, left: 12, right: 12, top: 40),
+          padding: const EdgeInsets.only(
+            bottom: 50,
+            left: 12,
+            right: 12,
+          ),
           height: MediaQuery.of(context).size.height,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
@@ -58,25 +65,22 @@ class _MCGPageState extends State<MCGPage> {
               Column(
                 children: [
                   SizedBox(
-                    height: 200,
+                    height: 300,
                     child: Column(
                       mainAxisSize: MainAxisSize.max,
                       children: [
                         Expanded(
                           child: TextFormField(
-                            key: Key(_mlHr.toString()),
-                            //initialValue: _mlHr.toString(),
                             style: const TextStyle(color: Colors.black),
                             keyboardType: TextInputType.number,
-                            inputFormatters: [heightFormatter],
-
+                            //inputFormatters: [heightFormatter],
                             decoration: InputDecoration(
-                              hintText: '0.00',
-                              suffixText: 'm',
+                              hintText: 'mL/h',
+                              suffixText: 'mL/h',
                               filled: true,
                               fillColor:
                                   const Color.fromARGB(23, 107, 102, 102),
-                              label: const Text('Altura'),
+                              label: const Text('mL/h'),
                               isDense: true,
                               labelStyle: const TextStyle(
                                   fontSize: 18,
@@ -88,29 +92,11 @@ class _MCGPageState extends State<MCGPage> {
                                 borderRadius: BorderRadius.circular(12),
                               ),
                             ),
-                            controller: _controllerHeight,
-                            onChanged: (IMCPageState) {
-                              if (_weight != 0) {
-                                setState(() {
-                                  if (_controllerHeight!.text != '') {
-                                    _mlHr =
-                                        double.parse(_controllerHeight!.text);
-                                  }
-                                  if (_controllerMlHr!.text != '') {
-                                    _weight =
-                                        double.parse(_controllerMlHr!.text);
-                                  }
-                                  _imc = (_weight! /
-                                      ((_mlHr! / 100) * (_mlHr! / 100)));
-                                });
-                              }
-                            },
+                            controller: _controllerMlHr,
                           ),
                         ),
                         Expanded(
                           child: TextFormField(
-                            key: Key(_formKey.toString()),
-                            //initialValue: _weight.toString(),
                             style: const TextStyle(color: Colors.black),
                             keyboardType: TextInputType.number,
                             decoration: InputDecoration(
@@ -131,104 +117,95 @@ class _MCGPageState extends State<MCGPage> {
                                 borderRadius: BorderRadius.circular(12),
                               ),
                             ),
-                            controller: _controllerMlHr,
-                            onChanged: (IMCPageState) {
-                              if (_mlHr! >= 0 && _weight! >= 0) {
-                                setState(() {
-                                  if (_controllerHeight!.text != '') {
-                                    _mlHr =
-                                        double.parse(_controllerHeight!.text);
-                                  }
-                                  if (_controllerMlHr!.text != '') {
-                                    _weight =
-                                        double.parse(_controllerMlHr!.text);
-                                  }
-                                  _imc = (_weight! / ((_mlHr)! * (_mlHr)!));
-                                });
-                              }
-                            },
+                            controller: _controllerWeight,
+                          ),
+                        ),
+                        Expanded(
+                          child: TextFormField(
+                            style: const TextStyle(color: Colors.black),
+                            keyboardType: TextInputType.number,
+                            decoration: InputDecoration(
+                              hintText: 'mg no soro',
+                              suffixText: 'mg',
+                              filled: true,
+                              fillColor:
+                                  const Color.fromARGB(23, 107, 102, 102),
+                              label: const Text('mg no Soro'),
+                              isDense: true,
+                              labelStyle: const TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black),
+                              enabledBorder: const OutlineInputBorder(
+                                  borderSide: BorderSide(color: Colors.black)),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(12),
+                              ),
+                            ),
+                            controller: _controllerMgSoro,
+                          ),
+                        ),
+                        Expanded(
+                          child: TextFormField(
+                            style: const TextStyle(color: Colors.black),
+                            keyboardType: TextInputType.number,
+                            decoration: InputDecoration(
+                              hintText: '250mL ou 500mL',
+                              suffixText: 'mL',
+                              filled: true,
+                              fillColor:
+                                  const Color.fromARGB(23, 107, 102, 102),
+                              label: const Text('Volume da bolsa'),
+                              isDense: true,
+                              labelStyle: const TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black),
+                              enabledBorder: const OutlineInputBorder(
+                                  borderSide: BorderSide(color: Colors.black)),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(12),
+                              ),
+                            ),
+                            controller: _controllerMlSoro,
                           ),
                         ),
                       ],
                     ),
                   ),
-                  if (_imc != 0)
-                    SizedBox(
-                      height: 100,
-                      child: Text(
-                        'O IMC do paciente é ${_imc?.toStringAsFixed(2)}',
-                        style: const TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black),
-                      ),
+                  if (_mcgKgMin != 0)
+                    Text(
+                      '${_mcgKgMin!.toStringAsFixed(2)} mcg/kg/min',
+                      style: const TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 18),
                     ),
-                  if (_imc! < 18.5 && _imc! > 0)
-                    const SizedBox(
-                      height: 100,
-                      child: Text(
-                        'Paciente com baixo peso',
-                        style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black),
-                      ),
-                    ),
-                  if (_imc! >= 18.5 && _imc! < 25)
-                    const SizedBox(
-                      height: 100,
-                      child: Text(
-                        'Paciente com peso normal',
-                        style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black),
-                      ),
-                    ),
-                  if (_imc! >= 25 && _imc! < 30)
-                    const SizedBox(
-                      height: 100,
-                      child: Text(
-                        'Paciente com excesso de peso',
-                        style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black),
-                      ),
-                    ),
-                  if (_imc! >= 30 && _imc! < 35)
-                    const SizedBox(
-                      height: 100,
-                      child: Text(
-                        'Paciente com obesidade de classe 1',
-                        style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black),
-                      ),
-                    ),
-                  if (_imc! >= 35 && _imc! < 40)
-                    const SizedBox(
-                      height: 100,
-                      child: Text(
-                        'Paciente com obesidade classe 2',
-                        style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black),
-                      ),
-                    ),
-                  if (_imc! >= 40)
-                    const SizedBox(
-                      height: 100,
-                      child: Text(
-                        'Paciente com obesidade classe 3',
-                        style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black),
-                      ),
-                    ),
+                  CustomWhiteButton(
+                      label: 'Calcular',
+                      onpressed: () {
+                        if (_mlHr! >= 0 &&
+                            _weight! >= 0 &&
+                            _mgSoro! >= 0 &&
+                            _mlSoro! >= 0) {
+                          setState(() {
+                            if (_controllerMlHr!.text != '') {
+                              _mlHr = double.parse(_controllerMlHr!.text);
+                            }
+                            if (_controllerWeight!.text != '') {
+                              _weight = double.parse(_controllerWeight!.text);
+                            }
+                            if (_controllerMgSoro!.text != '') {
+                              _mgSoro = double.parse(_controllerMgSoro!.text);
+                            }
+                            if (_controllerMlSoro!.text != '') {
+                              _mlSoro = double.parse(_controllerMlSoro!.text);
+                            }
+                            _mcgKgMin =
+                                ((((_mgSoro! / _mlSoro!) * _mlHr!) * 1000) /
+                                        _weight!) /
+                                    60;
+                          });
+                        }
+                      }),
                 ],
               ),
             ],
